f4adac75282813bbab537bebab5ff825ed050267
Ranjana.Yadav9@T-Mobile.com <Raj.Singh@OpusConsulting.com>
2020-04-22 17:51:32 +0530

    testing usernmae chages

--- src/components/Project/AddProject.js
+++ src/components/Project/AddProject.js
@@ -37,7 +37,6 @@ class  AddProject extends Component {
             end_date : this.state.end_date
         }
 
-        console.log(newProject)
         this.props.createProject(newProject, this.props.history)
     }
     
